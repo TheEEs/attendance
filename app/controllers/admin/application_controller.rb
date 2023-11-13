7785e093e2af9dcdf0dbbeae71df4b5e6d7b7f6b
Tran Dat <visualbasic2013@hotmail.com>
2023-11-14 01:34:31 +0700

    :sparkles: Allow detach many-to-many association records

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -25,5 +25,32 @@ module Admin
                         collection_field_name: collection_field_name, page: page, namespace: namespace, resource:
                         resource, table_title: "page-title"}
     end
+
+    def destroy
+      detach_params = self.detach_params
+      @detach = detach_params[:detach] == "true" and @detach_class = detach_params[:parent_class] and @detach_id = detach_params[:parent_id]
+      unless @detach 
+        if requested_resource.destroy
+          flash[:notice] = translate_with_resource("destroy.success")
+        else
+          flash[:error] = requested_resource.errors.full_messages.join("<br/>")
+        end
+        redirect_to after_resource_destroyed_path(requested_resource)
+      else
+        @detach_class = @detach_class.pluralize
+        if requested_resource.send(@detach_class).delete(@parent_record = Conference.find(@detach_id))
+          flash[:notice] = translate_with_resource("destroy.success")
+        else
+          flash[:error] = requested_resource.errors.full_messages.join("<br/>")
+        end
+        redirect_to url_for([namespace, @parent_record])
+      end
+    end
+
+    private 
+    def detach_params
+      params.permit(:detach, :parent_id, :parent_class)
+    end
+
   end
 end
--- app/models/delegate.rb
+++ app/models/delegate.rb
@@ -1,5 +1,5 @@
 class Delegate < ApplicationRecord
-  has_many :attends
+  has_many :attends, dependent: :destroy
   has_many :conferences, :through => :attends, inverse_of: :delegates
   validates :name, presence: true
   validates :department, presence: true
