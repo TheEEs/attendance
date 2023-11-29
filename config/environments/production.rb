d9635f0acc17357a47f237bbe94171e9903a0ce0
Tran Dat <visualbasic2013@hotmail.com>
2023-11-29 08:57:02 +0700

    :lock: Make session store persist for 1.days

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.session_store :cookie_store, expire_after: 14.days, key: '_attendance_cookie'
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
