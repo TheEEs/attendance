6f98d7a3fd6901d3cf637999958aa0fb482ed315
Tran Dat <visualbasic2013@hotmail.com>
2023-11-29 10:03:30 +0700

    :tada: Done Attend views and logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
   
-  get 'attend/:conference_id', to: "attend#index"
+  get 'attend/:conference_id', to: "attend#index", as: :show_attend
   get 'attend/:conference_id/attend', to: "attend#attend", as: :attend
   post 'attend/:conference_id/', to: "attend#make_attend", as: :make_attend
   namespace :admin do
