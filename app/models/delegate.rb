66ceb4666e505bf5b577e9e8065d2303dbc19704
Tran Dat <visualbasic2013@hotmail.com>
2023-12-01 18:45:06 +0700

    :building_construction: Allows Delegate's name to be blank

--- app/models/delegate.rb
+++ app/models/delegate.rb
@@ -1,7 +1,7 @@
 class Delegate < ApplicationRecord
   has_many :attends, dependent: :destroy
   has_many :conferences, :through => :attends, inverse_of: :delegates
-  validates :name, presence: true
+  #validates :name, presence: true
   validates :department, presence: true
 
   def display_title
